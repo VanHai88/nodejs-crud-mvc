be75974f8415254d4e0d340410536fc0a550e7b8
Van Hai <zzvanhai81@gmail.com>
2026-04-06 22:43:22 +0700

    CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
